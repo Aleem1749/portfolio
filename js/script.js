9376a7507985cb5d9e83c00a9e90294a025baccb
muhammadaleem <aleemmuhammad741@gmail.com>
2024-08-26 17:12:01 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// =======================typing animation=========================
+var typed = new Typed(".typing", {
+  strings: ["", "Web Designer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+// // =========================== function
+function showsidebar() {
+  document.getElementById("aside-2").style.display = "block";
+  document.getElementById("btn-2").style.display = "none";
+}
+// about page
+function aboutpage() {
+  document.getElementById("aside-2").style.display = "none";
+
+  document.getElementById("btn-2").style.display = "block";
+}
+// service
+function servicepage() {
+  document.getElementById("aside-2").style.display = "none";
+  document.getElementById("btn-2").style.display = "block";
+}
+
+function portfoliopage() {
+  document.getElementById("aside-2").style.display = "none";
+  document.getElementById("btn-2").style.display = "block";
+}
+
+function contactpage() {
+  document.getElementById("aside-2").style.display = "none";
+  document.getElementById("btn-2").style.display = "block";
+}
+
+function homepage() {
+  document.getElementById("aside-2").style.display = "none";
+  document.getElementById("btn-2").style.display = "block";
+}
